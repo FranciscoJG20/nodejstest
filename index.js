2a67bb35d4472b50709d29f278a2843503c28c80
Francisco <fjgutierrez112@gmail.com>
2019-01-29 19:50:00 -0500

    testing how modules work

---
+++ index.js
@@ -0,0 +1,7 @@
+var name = require("./module1");
+// you must import module in order to access it!
+console.log(name.a + " " + name.b);
+
+// console.log("hi, francisco!");
+// var a = 1;
+// console.log(a);
---
+++ module1.js
@@ -0,0 +1,6 @@
+var firstName = "Francisco";
+var lastName = "Gutierrez";
+
+module.exports.a = firstName;
+module.exports.b = lastName;
+// must export variable in order to use in index.js
